b25d731e4b63a072e0ebe35ccf65123df4479477
Taher <mhosseintaher@gmail.com>
2025-08-23 22:05:22 +0330

    WIP

--- nvim/.config/nvim/lua/taher/plugins/formatting.lua
+++ nvim/.config/nvim/lua/taher/plugins/formatting.lua
@@ -6,15 +6,15 @@ return {
 
         conform.setup({
             formatters = {
-                ["markdown-toc"] = {
-                    condition = function(_, ctx)
-                        for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-                            if line:find("<!%-%- toc %-%->") then
-                                return true
-                            end
-                        end
-                    end,
-                },
+                -- ["markdown-toc"] = {
+                --     condition = function(_, ctx)
+                --         for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+                --             if line:find("<!%-%- toc %-%->") then
+                --                 return true
+                --             end
+                --         end
+                --     end,
+                -- },
                 ["markdownlint-cli2"] = {
                     condition = function(_, ctx)
                         local diag = vim.tbl_filter(function(d)
@@ -24,7 +24,7 @@ return {
                     end,
                 },
             },
-            			formatters_by_ft = {
+                formatters_by_ft = {
 				javascript = { "biome-check" },
 				typescript = { "biome-check" },
 				javascriptreact = { "biome-check" },
@@ -42,11 +42,11 @@ return {
                 markdown = { "prettier", "markdown-toc" },
                 -- ["markdown.mdx"] = { "prettier", "markdownlint", "markdown-toc" },
             },
-            -- format_on_save = {
-            -- 	lsp_fallback = true,
-            -- 	async = false,
-            -- 	timeout_ms = 1000,
-            -- },
+            format_on_save = {
+            	lsp_fallback = true,
+            	async = false,
+            	timeout_ms = 1000,
+            },
         })
 
         -- Configure individual formatters
