d379f49a759406d4ea5e7e337d81d4524f296d08
Taher <mhosseintaher@gmail.com>
2025-08-24 10:05:45 +0330

    WIP

--- nvim/.config/nvim/lua/taher/plugins/telescope.lua
+++ nvim/.config/nvim/lua/taher/plugins/telescope.lua
@@ -50,7 +50,7 @@ return {
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-		vim.keymap.set("n", "<cmd>p", builtin.git_files, { desc = "My Always Control P" })
+		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "My Always Control P" })
 
 		vim.keymap.set("n", "<leader>s/", function()
 			builtin.live_grep({
